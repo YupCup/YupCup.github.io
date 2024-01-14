a687dfaf1a0be0b2f486b7d51edc3cf11e0825c2
Jacob Fossum Myrland <jacob.fossum.myrland@gmail.com>
2024-01-14 15:19:49 +0100

    Complete refactor
    allowing extension of lists without changing the board
    Weekend and Weekday list added (though their inclusion makes the board change again upon updates ugh)

---
+++ bingoContent.js
@@ -0,0 +1,64 @@
+bingoContent = [
+    "Hanna og Synnøve sier noe i kor",
+    "Adrian FO er skuffet over maten",
+    "Brannalarmen går",
+    "Skrymir jorder seg",
+    "Noen sender en snap med en doklage",
+    "Matkøen går forbi doen",
+    "Romskipelev bærer på noe rart",
+    "Halvparten av CG har på joggebukse",
+    "Kampmusikk på treningsrommet",
+    "Du får bare ris til restemiddag",
+    "Boffen spinner en frisbee på fingeren",
+    "Vebjørn lager grimaser",
+    "Rasmus OG Sami sitter på kunstrommet",
+    "Snorre spiller Beatsaber",
+    "Tegneserie tegner Hafsteinn",
+    "Allsang i dusjen",
+    "RM-soundtrack i vaskekjelleren",
+    "Vaskekjelleren er full",
+    "Du blir låst ute om kvelden",
+    "Dørene åpnes sent til mat",
+    "Feilmelding på tørketrommelen",
+    "Noen er i drag",
+    "Mikan mister noe",
+    "Emil klager over Dark Souls 2",
+    "Egg til frokost",
+    "Noen rager på Esport",
+    "Joya og John siterer Joya og John",
+    "Romskip jobber ekstremt overtid",
+    "Adele sitter uergonomisk",
+    "Daniel blir sint på fotball på hemsen",
+    "Kjøkkentjenesten ser døde ut",
+    "Esport Mjauer"
+];
+
+weekdayList = [
+    "'Du er en ..!' - Claus",
+    "Tove sier 'Gummisalen' eller 'bulkegjengen'",
+    "10 folk rekker ikke morgenmøte",
+    "Claus titter over brillene sine",
+    "Vegard ber folk ta føttene ned fra setene",
+    "Ord for dagen møter ikke opp",
+    "Hans-Thore-latter",
+    "Tove banner",
+    "Nytt Johann sitat",
+    "Ragnhild nevner barna sine og stønner tungt"
+];
+
+saturdayList = [
+    "Suppe til middag",
+    "Rage klager over grøt"
+];
+
+freeSpaces = [
+    "Jacob sier en dårlig vits",
+    "Skrymir spiller Mario Kart",
+    "Lærer Adrian jobber på rollespill i timen",
+    "CG gamer i timen",
+    "Ny sexvits i elev-quotes",
+    "Under 10 personer er våkne til frokost",
+    "Undergangdusjen er opptatt",
+    "Helene og Matteo er hyped over gatcha",
+    "Cosplay er tomme for gaffateip"
+];
