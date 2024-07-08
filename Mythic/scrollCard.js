a7e1a365ba09f59da683f39a9dd53e57d5d7b3f6
Jacob Fossum Myrland <jacob.fossum.myrland@gmail.com>
2024-07-08 22:20:41 +0200

    mobile scrolling

--- Mythic/scrollCard.js
+++ Mythic/scrollCard.js
@@ -3,7 +3,9 @@ for (let i = 0; i < cardHolders.length; i++) {
     let cardHolder = cardHolders[i];
     UpdateCardIndexes(cardHolder);
 
-    cardHolder.addEventListener("wheel", (eventInfo) => changeActiveCard(eventInfo, cardHolder));
+    cardHolder.addEventListener("wheel", (eventInfo) => changeActiveCardWheel(eventInfo, cardHolder));
+    cardHolder.addEventListener("touchstart", touchStart);
+    cardHolder.addEventListener("touchmove", (eventInfo) => changeActiveCardMouse(eventInfo, cardHolder));
 }
 
 function UpdateCardIndexes(cardHolder) {
@@ -14,16 +16,42 @@ function UpdateCardIndexes(cardHolder) {
     cardHolder.style.setProperty("--scroll-amount", 0);
 }
 
+let start = {x:0, y:0};
+function touchStart(event) {
+    start.x = event.touches[0].pageX;
+    start.y = event.touches[0].pageY;
+}
 
-function changeActiveCard(eventInfo, cardHolder) {
+function touchMove(event) {
+    offset = {};
+
+    offset.x = start.x - event.touches[0].pageX;
+    offset.y = start.y - event.touches[0].pageY;
+
+    return offset;  
+}
+
+function changeActiveCardMouse(eventInfo, cardHolder) {
+    changeActiveCard(cardHolder, parseInt(touchMove(eventInfo).x) * -5);
+    touchStart(eventInfo);
+}
+
+function changeActiveCardWheel(eventInfo, cardHolder) {
+    changeActiveCard(cardHolder, eventInfo.wheelDelta);
+}
+
+function changeActiveCard(cardHolder, delta) {
+    if (isNaN(delta))
+        return;
+
     let scrollAmount = parseInt(cardHolder.style.getPropertyValue("--scroll-amount"));
     if (isNaN(scrollAmount))
         scrollAmount = 0;
-    scrollAmount -= eventInfo.wheelDelta;
+    scrollAmount -= delta;
 
     let scrollSpeed = getComputedStyle(cardHolder).getPropertyValue("--scroll-speed");
     if (scrollAmount < 0 || cardHolder.children.length < scrollAmount/scrollSpeed + 1)
-        scrollAmount += eventInfo.wheelDelta;
+        scrollAmount += delta;
 
     cardHolder.style.setProperty("--scroll-amount", scrollAmount);
 
